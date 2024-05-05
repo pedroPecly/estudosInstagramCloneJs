0f8fc9692419939ac1e627a62ff9bd20c831da06
pedroPecly <p.pecly@gsuite.iff.edu.br>
2024-05-05 19:36:13 -0300

    no message

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDPAawZ-fcrW6HzrMW6NqhD5A_b9HmZ2q0",
-    authDomain: "instagram-clone-estudos-df984.firebaseapp.com",
-    projectId: "instagram-clone-estudos-df984",
-    storageBucket: "instagram-clone-estudos-df984.appspot.com",
-    messagingSenderId: "274836158736",
-    appId: "1:274836158736:web:62f4a53a0a2774852d0d95",
-    measurementId: "G-P9XVHB6MXC"
+  //a configurar no firebase
   });
 
 const db = firebase.firestore();
